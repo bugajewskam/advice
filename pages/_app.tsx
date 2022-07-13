740310aa2f1618b3b8a0191c6e98428bc3e4f9db
Monika Bugajewska <bugajewska.m@gmail.com>
2022-07-13 15:13:33 +0200

    add responsive view

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,27 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Head>
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin/>
-<link href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,100&family=Manrope:wght@800&display=swap" rel="stylesheet"/>
-    </Head>
-    <Component {...pageProps} />
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+        />
+        <meta httpEquiv="X-UA-Compatible" content="ie=edge"></meta>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Josefin+Sans:ital,wght@1,100&family=Manrope:wght@800&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
+      <Component {...pageProps} />
     </>
-  )
-    
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -21,20 +21,36 @@ const Home: NextPage = () => {
 
         {advice && (
           <div>
-           <div className="number"> <h5>ADVICE #{advice?.id}</h5></div>
+            <div className="number">
+              {" "}
+              <h5>ADVICE #{advice?.id}</h5>
+            </div>
             <div className="advice">{advice?.advice}</div>
           </div>
         )}
         <div className="kreska">
-          <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
-            <g fill="none" fill-rule="evenodd">
-              <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
-              <g transform="translate(212)" fill="#CEE3E9">
-                <rect width="6" height="16" rx="3" />
-                <rect x="14" width="6" height="16" rx="3" />
-              </g>
-            </g>
-          </svg>
+
+              {/* <svg width="295" height="16" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fill-rule="evenodd">
+                  <path fill="#4F5D74" d="M0 8h122v1H0zM173 8h122v1H173z" />
+                  <g transform="translate(138)" fill="#CEE3E9">
+                    <rect width="6" height="16" rx="3" />
+                    <rect x="14" width="6" height="16" rx="3" />
+                  </g>
+                </g>
+              </svg> */}
+
+              <svg width="444" height="16" xmlns="http://www.w3.org/2000/svg">
+                <g fill="none" fill-rule="evenodd">
+                  <path fill="#4F5D74" d="M0 8h196v1H0zM248 8h196v1H248z" />
+                  <g transform="translate(212)" fill="#CEE3E9">
+                    <rect width="6" height="16" rx="3" />
+                    <rect x="14" width="6" height="16" rx="3" />
+                  </g>
+                </g>
+              </svg>
+            
+          
         </div>
       </div>
 
